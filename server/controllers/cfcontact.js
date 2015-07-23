c4d5ba5f3e2a9114792ab9617aa43beea89a0d44
Gabriel Pedro <gpedro831@gmail.com>
2015-07-22 23:19:44 -0400

    Add ContactForm

---
+++ server/controllers/cfcontact.js
@@ -0,0 +1,63 @@
+var _ = require('lodash');
+
+module.exports = {
+
+  create: function create(req, res) {
+    if (!res.locals.template) res.locals.template = res.locals.model + '/' + 'create';
+
+    if (!res.locals.record) res.locals.record = {};
+
+    _.merge(res.locals.record, req.query);
+
+    if (req.method === 'POST') {
+      if (req.isAuthenticated()) req.body.creatorId = req.user.id;
+
+      res.locals.messageSend = false;
+      // set temp record for use in validation errors
+      res.locals.record = req.query;
+      _.merge(res.locals.record, req.body);
+
+      return res.locals.Model.create(req.body)
+      .then(function (record) {
+        res.locals.record = record;
+        res.locals.messageSend = true;
+
+        var templateVariables = {
+          user: record,
+          site: {
+            name: we.config.appName,
+            url: we.config.hostname
+          }
+        };
+
+        var options = {
+          subject: req.__('we.email.ContactConfirmation.subject', templateVariables),
+          email: user.email
+        };
+
+        we.email.sendEmail('ContactConfirmation', options, templateVariables, function (err) {
+          if (err) {
+            we.log.error('Action:ContactConfirmation sendEmail:', err);
+            return res.serverError();
+          }
+
+        });
+
+        res.status(200);
+        res.ok();
+
+      }).catch(res.queryError);
+    } else {
+      res.locals.record = req.query;
+
+      if (req.isAuthenticated()) {
+        res.locals.record.name = req.user.displayName;
+        res.locals.record.email = req.user.email;
+      }
+
+      res.status(200);
+      res.ok();
+    }
+  }
+
+};
---
+++ server/models/cfcontact.js
@@ -0,0 +1,40 @@
+/**
+ * Conferece Contact model
+ *
+ * @module      :: Model
+ * @description :: Store conference contacts
+ */
+ module.exports = function Model(we) {
+  var model = {
+    definition: {
+      conferenceId: {
+        type: we.db.Sequelize.BIGINT,
+        allowNull: false,
+        formFieldType: null
+      },
+      creatorId: {
+        type: we.db.Sequelize.BIGINT,
+        formFieldType: null
+      },
+      name: {
+        type:  we.db.Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: we.db.Sequelize.STRING,
+        formFieldType: 'email',
+        validate: { isEmail: true },
+        allowNull: false
+      },
+      message: {
+        type: we.db.Sequelize.TEXT,
+        formFieldType: 'textarea',
+        formFieldAttributes: {
+          rows: 6
+        },
+        allowNull: false
+      }
+    }
+  }
+  return model;
+ };
