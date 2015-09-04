c2b174cd450909a8a4e6a5a51a319b0bb47d68e5
albertosouza <alberto.souza.99@gmail.com>
2015-09-04 09:28:18 -0300

    add install.js

---
+++ install.js
@@ -0,0 +1,25 @@
+module.exports = {
+  /**
+   * Return a list of updates
+   *
+   * @param  {Object} we we.js object
+   * @return {Array}    a list of update objects
+   */
+  updates: function updates() {
+    return [
+      {
+        version: '0.3.27',
+        /**
+         * Add present column
+         */
+        update: function update0327(we, done) {
+          var sql = 'ALTER TABLE `cfregistrations` ADD '+
+            ' COLUMN `present` TINYINT(1) NOT NULL DEFAULT 0;';
+          we.db.defaultConnection.query(sql).then(function(){
+            done();
+          }).catch(done);
+        }
+      }
+    ];
+  }
+};
