2e2e3c00e720bdca2352006fa8b636a932747452
albertosouza <alberto.souza.99@gmail.com>
2015-09-13 14:24:35 -0300

    add event date range helper

---
+++ server/helpers/we-cf-date-range.js
@@ -0,0 +1,23 @@
+/**
+ * We.js event date range
+ *
+ * render one date range
+ *
+ * usage:  {{we-cf-date-range start=date end=date __=__}}
+ */
+module.exports = function(we) {
+  return function renderDateRange() {
+    var options = arguments[arguments.length-1];
+
+    var start = we.utils.moment(options.hash.start);
+    var end = we.utils.moment(options.hash.end);
+
+    // dd/mm a dd/mm de aaaa
+    return new we.hbs.SafeString(options.hash.__('event.date.range', {
+      start: start.format('DD/MM'),
+      end: end.format('DD/MM'),
+      yearStart: start.format('YYYY'),
+      yearEnd: end.format('YYYY')
+    }));
+  }
+};
