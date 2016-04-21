ac05f85f0765a3feae2e73a6d9938283ab650f39
albertosouza <alberto.souza.99@gmail.com>
2016-04-21 20:34:12 -0300

    add cfpage tests

--- server/controllers/cfpage.js
+++ server/controllers/cfpage.js
@@ -1,45 +1,22 @@
 module.exports = {
-  find: function find(req, res, next) {
-    if (!res.locals.event) return next();
-
+  find: function find(req, res) {
     res.locals.query.where.eventId = res.locals.event.id;
 
-    return res.locals.Model.findAndCountAll(res.locals.query)
-    .then(function (record) {
-      if (!record) return next();
+    res.locals.Model.findAll(res.locals.query)
+    .then(function afterFind(record) {
+      res.locals.data = record;
 
-      res.locals.metadata.count = record.count;
-      res.locals.data = record.rows;
+      res.locals.Model.count(res.locals.query)
+      .then(function afterCount(count) {
 
-      return res.ok();
-    });
-  },
-  findOne: function findOne(req, res, next) {
-    if (!res.locals.data) return next();
+        res.locals.metadata.count = count;
+        return res.ok();
 
-    if (req.params.eventId != res.locals.data.eventId)
-      return next();
-
-    req.we.hooks.trigger('we:after:send:ok:response', {
-      res: res, req: req
-    }, function (err) {
-      if (err) return res.serverError(err);
-      return res.ok();
-    });
+      }).catch(res.queryError);
+    }).catch(res.queryError);
   },
   managePage: function managePage(req, res, next) {
-    if (!res.locals.event) return next();
-
-    res.locals.query.where.eventId = res.locals.event.id;
-
-    return res.locals.Model.findAndCountAll(res.locals.query)
-    .then(function (record) {
-      if (!record) return res.notFound();
-
-      res.locals.metadata.count = record.count;
-      res.locals.data = record.rows;
-
-      return res.ok();
-    });
+    // send to find action, this allows to set custom permissions and template for this action but with find logic
+    req.we.controllers.cfpage.find(req, res, next);
   }
 };
