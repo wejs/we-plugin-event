c4db47e6589e64546d328dc37ce948dbc80acacd
albertosouza <alberto.souza.99@gmail.com>
2016-04-22 17:02:06 -0300

    add cfroom controller test

--- server/controllers/cfroom.js
+++ server/controllers/cfroom.js
@@ -1,87 +1,67 @@
 module.exports = {
-  find: function find(req, res, next) {
-    if (!res.locals.event) return res.notFound();
-
+  find: function find(req, res) {
     res.locals.query.where.eventId = res.locals.event.id;
 
-    return res.locals.Model.findAndCountAll(res.locals.query)
-    .then(function (record) {
-      if (!record) return next();
+    res.locals.Model.findAll(res.locals.query)
+    .then(function afterFind(record) {
 
-      res.locals.metadata.count = record.count;
-      res.locals.data = record.rows;
+      res.locals.data = record;
 
-      return res.ok();
-    });
+      res.locals.Model.findAll(res.locals.query)
+      .then(function afterCount(count) {
+        res.locals.metadata.count = count;
+        return res.ok();
+      }).catch(res.queryError);
+    }).catch(res.queryError);
   },
   create: function create(req, res) {
     if (!res.locals.data) res.locals.data = {};
 
-    res.locals.data.eventId = req.params.eventId;
+    res.locals.data.eventId = res.locals.event.id;
 
     if (req.method === 'POST') {
 
       if(req.isAuthenticated()) req.body.creatorId = req.user.id;
       req.body.eventId = req.params.eventId;
 
-      res.locals.data = req.query;
       req.we.utils._.merge(res.locals.data, req.body);
 
       return res.locals.Model.create(req.body)
-      .then(function (record) {
+      .then(function afterCreate(record) {
         res.locals.data = record;
-        if (res.locals.responseType == 'html')
-          return res.redirect(
+        if (req.accepts('html')) {
+          return res.goTo(
             '/event/' + res.locals.event.id + '/admin/room'
           );
+        }
         res.created();
       }).catch(res.queryError);
     } else {
-      res.locals.data = req.query;
       res.ok();
     }
   },
   edit: function edit(req, res) {
-    var we = req.getWe();
+    if(!res.locals.data) return res.notFound();
 
-    we.db.models.cfroom.findOne({where: {
-        id: req.params.cfroomId
-      }}).then(function (record) {
-      if (!record) return res.notFound();
+    if (req.method == 'POST' || req.method == 'PUT') {
+      delete req.body.eventId;
 
-      res.locals.data = record;
-
-      if (req.method == 'POST' || req.method == 'PUT') {
-
-        req.body.eventId = req.params.eventId;
-
-        record.updateAttributes(req.body)
-        .then(function() {
-        if (res.locals.responseType == 'html')
-          return res.redirect(
+      res.locals.data.updateAttributes(req.body)
+      .then(function afterUpdate() {
+        if (req.accepts('html')) {
+          return res.goTo(
             '/event/' + res.locals.event.id + '/admin/room'
           );
-          res.ok();
-        }).catch(res.queryError);
-      } else {
-        res.ok();
-      }
-    }).catch(res.queryError);
+        }
+        res.updated();
+      }).catch(res.queryError);
+    } else {
+      res.ok();
+    }
+
   },
-  managePage: function managePage(req, res) {
-    if (!res.locals.event) return res.notFound();
-
-    res.locals.query.where.eventId = res.locals.event.id;
-
-    return res.locals.Model.findAndCountAll(res.locals.query)
-    .then(function (record) {
-      if (!record) return res.notFound();
-
-      res.locals.metadata.count = record.count;
-      res.locals.data = record.rows;
-
-      return res.ok();
-    });
+  managePage: function managePage(req, res, next) {
+    // send to find action, this allows to set custom permissions and template for this action but with find logic
+    req.we.controllers.cfroom.find(req, res, next);
   }
-
 };
---
+++ test/1_unit/controller_cfroom.test.js
@@ -0,0 +1,363 @@
+var assert = require('assert');
+var helpers = require('we-test-tools').helpers;
+var controller, we;
+
+describe('controller_cfroom', function () {
+  before(function (done) {
+    controller = require('../../server/controllers/cfroom.js');
+    we = helpers.getWe();
+
+    done();
+  });
+
+  describe('create', function() {
+    it('cfroom.create should run res.ok if not in POST request', function (done) {
+
+      var req = {
+        query: {},
+        method: 'GET',
+        we: we
+      };
+
+      var res = {
+        locals: {
+          event: { id: 12 }
+        },
+        ok: function() {
+          done();
+        }
+      };
+
+      controller.create(req, res);
+    });
+
+    it('cfroom.create should run res.goTo in html response after create the record', function (done) {
+
+      var req = {
+        method: 'POST', we: we,
+        paramsArray: [ 1 ],
+        params: { eventId: 4 },
+        body: {
+          name: 'Some teaser',
+          about: 'Any text'
+        },
+        isAuthenticated: function() {
+          return true;
+        },
+        user: { id: 1 },
+        accepts: function(format) {
+          assert.equal(format, 'html');
+          return true;
+        }
+      };
+      var res = {
+        addMessage: function(status, opts) {
+          assert.equal(status, 'success');
+          assert.equal(opts.text, 'cfroom.create.success');
+          assert(opts.vars.record);
+        },
+        locals: {
+          event: { id: 1 },
+          data: null,
+          Model:  {
+            create: function() {
+              return new we.db.Sequelize.Promise(function (resolve) {
+                resolve({
+                  id: 99
+                });
+              });
+            }
+          }
+        },
+        goTo: function(path) {
+          assert.equal(path, '/event/1/admin/room');
+          done();
+        }
+      };
+
+      controller.create(req, res, function(){});
+    });
+
+    it('cfroom.create should run res.created in json response after create the record', function (done) {
+
+      var req = {
+        method: 'POST', we: we,
+        params: { eventId: 4 },
+        body: {
+          name: 'Some teaser',
+          about: 'Any text'
+        },
+        isAuthenticated: function() {
+          return true;
+        },
+        user: { id: 1 },
+        accepts: function(format) {
+          assert.equal(format, 'html');
+          return false;
+        }
+      };
+      var res = {
+        addMessage: function(status, opts) {
+          assert.equal(status, 'success');
+          assert.equal(opts.text, 'cfroom.create.success');
+          assert(opts.vars.record);
+        },
+        locals: {
+          event: { id: 1 },
+          data: null,
+          Model:  {
+            create: function() {
+              return new we.db.Sequelize.Promise(function (resolve) {
+                resolve({
+                  id: 100
+                });
+              });
+            }
+          }
+        },
+        created: function() {
+          assert(res.locals.data.id);
+          done();
+        }
+      };
+
+      controller.create(req, res, function(){});
+    });
+  });
+
+  describe('edit', function() {
+    var RoomFindOne;
+    before(function(done) {
+      RoomFindOne = we.db.models.cfroom.findOne;
+      we.db.models.cfroom.findOne = function() {
+        return new we.db.Sequelize.Promise(function (resolve) {
+          resolve({
+            id: 10
+          });
+        });
+      }
+      done();
+    });
+    after(function(done) {
+      we.db.models.cfroom.findOne = RoomFindOne;
+      done();
+    });
+
+    it('cfroom.edit should run res.notFound without reloaded record', function (done) {
+
+      var req = { we: we };
+      var res = {
+        locals: { data: null },
+        notFound: function() { done(); }
+      };
+
+      controller.edit(req, res);
+    });
+
+    it('cfroom.edit should run res.ok for GET requests', function (done) {
+      var req = { method: 'GET', we: we };
+      var res = {
+        locals: {
+          data: { id: 1 },
+        },
+        ok: function() {
+          done();
+        }
+      };
+
+      controller.edit(req, res);
+    });
+
+    it('cfroom.edit should run res.goTo in html response after update', function (done) {
+
+      var req = {
+        method: 'POST', we: we,
+        paramsArray: [ 1 ],
+        body: {
+          name: 'Some teaser',
+          about: 'Alberto Souza site'
+        },
+        user: { id: 1 },
+        accepts: function(format) {
+          assert.equal(format, 'html');
+          return true;
+        }
+      };
+      var res = {
+        addMessage: function(status, opts) {
+          assert.equal(status, 'success');
+          assert.equal(opts.text, 'cfroom.updated.success');
+          assert(opts.vars.record);
+        },
+        locals: {
+          event: { id: 1 },
+          data: {
+            id: 10,
+            updateAttributes: function(data) {
+              assert(!data.eventId);
+              assert(!data.creatorId);
+              return new we.db.Sequelize.Promise(function (resolve) {
+                resolve();
+              });
+            },
+            toJSON: function(){ return this; }
+          }
+        },
+        goTo: function(path) {
+          assert.equal(path, '/event/1/admin/room');
+          done();
+        }
+      };
+
+      controller.edit(req, res);
+    });
+
+    it('cfroom.edit should run res.updated in json response after update', function (done) {
+
+      var req = {
+        method: 'POST', we: we,
+        params: { cfmenuId: 4 },
+        body: {
+          name: 'Some teaser',
+          about: 'Any text'
+        },
+        user: { id: 1 },
+        accepts: function(format) {
+          assert.equal(format, 'html');
+          return false;
+        }
+      };
+      var res = {
+        addMessage: function(status, opts) {
+          assert.equal(status, 'success');
+          assert.equal(opts.text, 'cfroom.updated.success');
+          assert(opts.vars.record);
+        },
+        locals: {
+          event: { id: 1 },
+          data: {
+            updateAttributes: function(data) {
+              assert(!data.eventId);
+              assert(!data.creatorId);
+              return new we.db.Sequelize.Promise(function (resolve) {
+                resolve();
+              });
+            },
+            toJSON: function(){ return this; }
+          }
+        },
+        updated: function() {
+          done();
+        }
+      };
+
+      controller.edit(req, res);
+    });
+  });
+
+  // describe('find', function() {
+  //   it('cfroom.find should find, count and run res.ok', function (done) {
+  //     var req = {};
+  //     var res = {
+  //       locals: {
+  //         event: { id: 10 },
+  //         data: null,
+  //         metadata: {},
+  //         query: {
+  //           where: {
+  //             something: 'the query'
+  //           }
+  //         },
+  //         Model: {
+  //           findAll: function(opts) {
+  //             assert.equal(opts.where.something, res.locals.query.where.something);
+
+  //             return new we.db.Sequelize.Promise(function (resolve) {
+  //               resolve([]);
+  //             });
+  //           },
+  //           count: function(opts) {
+  //             assert.equal(opts, res.locals.query);
+
+  //             return new we.db.Sequelize.Promise(function (resolve) {
+  //               resolve(0);
+  //             });
+  //           }
+  //         }
+  //       },
+  //       ok: function() {
+  //         done();
+  //       }
+  //     };
+
+  //     controller.find(req, res);
+  //   });
+  //   // it('cfroom.findOne should run res.goTo for html request', function (done) {
+
+  //   //   var req = {
+  //   //     params: {
+  //   //       cfmenuId: 1
+  //   //     },
+  //   //     accepts: function(format) {
+  //   //       assert.equal(format, 'html');
+  //   //       return true;
+  //   //     },
+  //   //     method: 'GET', we: we
+  //   //   };
+  //   //   var res = {
+  //   //     locals: {
+  //   //       event: {
+  //   //         id: 3
+  //   //       },
+  //   //       data: { id: 1 } },
+  //   //     goTo: function(path) {
+  //   //       assert.equal(path, '/event/3'+
+  //   //         '/admin/cfmenu/1/edit');
+  //   //       done();
+  //   //     }
+  //   //   };
+
+  //   //   controller.findOne(req, res, function (){});
+  //   // });
+
+  //   // it('cfroom.findOne should run res.ok for json request', function (done) {
+
+  //   //   var req = {
+  //   //     params: {
+  //   //       cfmenuId: 1
+  //   //     },
+  //   //     accepts: function(format) {
+  //   //       assert.equal(format, 'html');
+  //   //       return false; // is json
+  //   //     },
+  //   //     method: 'GET', we: we
+  //   //   };
+  //   //   var res = {
+  //   //     locals: {
+  //   //       event: { id: 3 },
+  //   //       data: { id: 1 }
+  //   //     },
+  //   //     ok: function() {
+  //   //       done();
+  //   //     }
+  //   //   };
+
+  //   //   controller.findOne(req, res, function (){});
+  //   // });
+  // });
+
+  describe('managePage', function() {
+    it('cfroom.managePage should run find controller', function (done) {
+      var find = we.controllers.cfroom.find;
+      we.controllers.cfroom.find = function () {
+        we.controllers.cfroom.find = find;
+        done();
+      }
+
+      var req = { we: we };
+      var res = {};
+
+      controller.managePage(req, res);
+    });
+  });
+});
